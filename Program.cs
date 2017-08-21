f38aa71a73a2d50de7be3937dddbf998ca23dece
REGINALDO MORIKAWA <reginaldo.morikawa@senacsp.edu.br>
2017-08-21 15:46:21 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,33 @@ namespace CompararStrings
     {
         static void Main(string[] args)
         {
+            string nome1, nome2;
+
+            Console.Write("Digite um nome: ");
+
+            nome1 = Console.ReadLine();
+            Console.Write("Digite outro nome: ");
+
+            nome2 = Console.ReadLine();
+
+            if (nome1 == nome2)
+
+            {
+
+                Console.Write("O nomes são iguais.");
+
+            }
+            else
+
+            {
+
+                Console.Write("Os nomes são diferentes.");
+
+            }
+
+
+
+            Console.ReadLine();
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+"Compara 2 Strings" 
